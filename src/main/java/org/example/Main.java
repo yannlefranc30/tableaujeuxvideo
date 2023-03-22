25513f35555a4a6d3b82be13a5caa40ff1dab263
yann <yannlefranc330@gmail.com>
2023-03-22 14:00:42 +0100

    tableauxjeuxvideo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+
+package org.example;
+
+//declaration jeux video
+
+
+public class Main {
+    public static void main(String[] args) {
+        String[] jeux = {"The witcher", "tombraider", "starwars"};
+        System.out.println(jeux[0]);
+    }
+}
+
+
+
